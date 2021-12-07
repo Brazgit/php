45fe72d4daf1da7d8b487a008eb111aaf9d12583
João Braz <95686675+Brazgit@users.noreply.github.com>
2021-12-07 09:10:34 -0300

    atualização nos campos

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Meu primeiro arqquivo</title>
     </head>
     <body>
         <?php        
             echo "Meu primeiro código PHP - Acessando o Git!";
+            echo "Teste de confrito!"
         ?>
     </body>
 </html>
