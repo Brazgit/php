1462e0cc52c77ef263b50ae05ffde3c21c71a922
Joao Braz <jjuniorbboy1930@gmail.com>
2021-12-07 08:39:44 -0300

    'mensagem'

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        // request - faz uma solicitação ao servidor
+        // response - reposta entre cliente e servidor
+        
+            echo "Meu primeiro código PHP - Acessando o Git!<p><strong>Hello Word!</strong>";
+        ?>
+    </body>
+</html>
